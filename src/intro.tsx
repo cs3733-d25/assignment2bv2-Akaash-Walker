ebfb613fd0fc8e16119b4629f221113d36234ae2
Owen Miller <ormiller@wpi.edu>
2025-04-02 16:37:53 -0400

    updated title and intro

--- src/intro.tsx
+++ src/intro.tsx
@@ -5,7 +5,7 @@ function Intro(props: Name) {
         <div>
             <p>My name is {props.name1} and I am a sophomore at WPI. I like to play
                 volleyball and I am excited to learn React!</p>
-            <p>My name is {props.name2}</p>
+            <p>My name is {props.name2} and I am a sophomore at WPI. I like to sail and ski and I am excited to master git!</p>
         </div>
     );
 }
--- src/title.tsx
+++ src/title.tsx
@@ -1,7 +1,7 @@
 function Title() {
     return (
         <header>
-            <h2>CS 3733-D25 Team 45: Second hand shopping and ________</h2>
+            <h2>CS 3733-D25 Team 45: Second hand shopping and Guitar Playing</h2>
         </header>
     );
 }
